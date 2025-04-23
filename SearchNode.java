5e0426914c69173a4d3ebf33935fa0b674bc9dc4
Varn1ka <157673718+Varn1ka@users.noreply.github.com>
2025-04-23 09:15:32 +0530

    Create SearchNode.java

---
+++ SearchNode.java
@@ -0,0 +1,31 @@
+class Node {
+    int data;
+    Node left, right;
+    Node(int item){
+        data = item;
+        left = right = null;
+    }
+}
+class SearchTree{
+    static class TreeNode{
+        int data;
+        TreeNode left, right;
+        TreeNode(int value){
+            data=value;
+            left=right=null;
+        }
+    }
+    public static Boolean search(TreeNode root,int k){
+        if(root==null) return false;
+        if(root.data==k)return true;
+        return search(root.left,k)||search(root.right,k);
+    }
+    public static void main(String[]args){
+        TreeNode root=new TreeNode(7);
+        root.left=new TreeNode(5);
+        root.right=new TreeNode(3);
+        root.left.left=new TreeNode(112);
+        System.out.println("Is 3 in tree?:"+search(root,3));
+        System.out.println("Is 19 in tree?:"+search(root,19));
+    }
+}
