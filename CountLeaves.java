7a8fc261f2bc37115d37b370c57bac4c42ccd1b8
Varn1ka <157673718+Varn1ka@users.noreply.github.com>
2025-04-23 09:17:22 +0530

    Create CountLeaves.java

---
+++ CountLeaves.java
@@ -0,0 +1,21 @@
+class g27{
+    static class TreeNode{
+        int data;
+        TreeNode left,right;
+        TreeNode(int val){
+            data=val;
+            left=right=null;
+        }
+    }
+    public static int countLeaves(TreeNode root){
+        if(root==null) return 0;
+        if(root.left==null&root.right==null)return 1;
+        return countLeaves(root.left)+countLeaves(root.right);
+    }
+    public static void main(String[]args){
+        TreeNode root=new TreeNode(12);
+        root.left=new TreeNode(24);
+        root.right=new TreeNode(36);
+        System.out.println("Number of Leaves in Binary Tree is "+countLeaves(root));
+    }
+}
